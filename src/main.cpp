f9353c1d7a76881c3c5e20ecde226b1b9182f6d9
Piotr Chrusciel <chrusciel.piotr.87@gmail.com>
2022-07-21 11:56:48 +0200

    Init

---
+++ src/main.cpp
@@ -0,0 +1,163 @@
+#include <Arduino.h>
+#include <Servo.h>
+#include <Wire.h>
+
+Servo hServo;
+Servo vServo;
+
+byte dataArray[16];
+
+#define SLAVE_ADDRESS 0x08
+#define H_SERVO_PIN 5
+#define V_SERVO_PIN 3
+
+int MOVE_TO_CMD = 1;
+int SET_IDLE = 2;
+int RESET_POS = 3;
+
+void receiveEvent(int howMany);
+void moveTo(Servo &servo, int position);
+void runIdle();
+
+bool idleMove = false;
+bool idleMoveRight = false;
+bool idleMoveUp = false;
+
+bool resetPos = false;
+
+int hMax = 180;
+int hMin = 0;
+
+int vMax = 115;
+int vMin = 70;
+
+int startingPosH = 0;
+int startingPosV = 70;
+
+int newPosH = startingPosH;
+int newPosV = startingPosV;
+
+void setup() {
+  Serial.begin(9600);
+
+  hServo.attach(H_SERVO_PIN);
+  vServo.attach(V_SERVO_PIN);
+
+  Wire.begin(SLAVE_ADDRESS);
+  Wire.onReceive(receiveEvent);
+}
+
+void receiveEvent(int howMany)
+{
+  int cmd = Wire.read();
+  int servoNo = Wire.read();
+  int position = Wire.read();
+  
+  Servo *servo;
+
+  if (servoNo == 1) {
+    servo = &hServo;
+
+    if (position > hMax) {
+      position = hMax;
+    }
+    if (position < hMin)
+    {
+      position = hMin;
+    }
+  } else {
+    servo = &vServo;
+    if (position > vMax)
+    {
+      position = vMax;
+    }
+    if (position < vMin)
+    {
+      position = vMin;
+    }
+  }
+
+  if (cmd == MOVE_TO_CMD) {
+    idleMove = false;
+    (*servo).write(position);
+  }
+  
+  if (cmd == SET_IDLE)
+  {
+    resetPos = true;
+    idleMove = true;
+  }
+
+  if (cmd == RESET_POS)
+  {
+    idleMove = false;
+    resetPos = true;
+  }
+}
+
+void moveTo(Servo &servo, int position) {
+  servo.write(position);
+}
+
+void runIdle()
+{
+  if (idleMoveRight) {
+    if (newPosH <= hMax) {
+      hServo.write(newPosH);
+      newPosH++;
+    } else {
+      newPosH = hMax;
+      idleMoveRight = false;
+    }
+  } else {
+    if (newPosH >= hMin) {
+      hServo.write(newPosH);
+      --newPosH;
+    } else {
+      newPosH = hMin;
+      idleMoveRight = true;
+    }
+  }
+
+  if (idleMoveUp) {
+    if (newPosV <= vMax) {
+      vServo.write(newPosV);
+      newPosV++;
+    } else {
+      newPosV = vMax;
+      idleMoveUp = false;
+    }
+  } else {
+    if (newPosV >= vMin) {
+      vServo.write(newPosV);
+      --newPosV;
+    } else {
+      newPosV = vMin;
+      idleMoveUp = true;
+    }
+  }
+
+  delay(300);
+}
+
+void reset() {
+  hServo.write(hMin);
+  delay(1000);
+  vServo.write(vMin);
+  delay(1000);
+
+  idleMoveRight = true;
+  idleMoveUp = true;
+  resetPos = false;
+}
+
+void loop() {
+  if (resetPos) {
+    reset();
+  }
+
+  if (idleMove) {
+    runIdle();
+  }
+  
+}
