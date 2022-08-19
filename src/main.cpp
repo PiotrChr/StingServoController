6c1c8c9b889f1285007b427a3207f84d12545487
Piotr Chrusciel <chrusciel.piotr.87@gmail.com>
2022-08-19 13:06:14 +0200

    Add axis toggle, idle axis speed and stop commands

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,9 @@ static int CMD_RESET_POS = 3;
 static int CMD_AUTO_IDLE_ON = 4;
 static int CMD_AUTO_IDLE_OFF = 5;
 static int CMD_STEP = 6;
+static int CMD_IDLE_AXIS = 7;
+static int CMD_IDLE_SPEED = 8;
+static int CMD_STOP = 9;
 
 void receiveEvent(int howMany);
 void runIdle();
@@ -27,6 +30,14 @@ bool idleMoveRight = false;
 bool idleMoveUp = false;
 bool resetPos = false;
 bool autoIdle = false;
+
+bool idleV = true;
+bool idleH = true;
+
+
+volatile int idleSpeed = 300;
+volatile int idleVSpeed;
+volatile int idleHSpeed;
 
 const int hMax = 180;
 const int hMin = 0;
@@ -41,7 +52,7 @@ volatile int newPosH;
 volatile int newPosV;
 
 volatile unsigned long lastAction;
-const unsigned long triggerIdleAfter = 120;
+const unsigned long triggerIdleAfter = 60000; // 6s
 
 void setup() {
   newPosH = startingPosH;
@@ -69,8 +80,10 @@ void showPos() {
 void receiveEvent(int howMany)
 {
   int cmd = Wire.read();
-  int servoNo = Wire.read();
-  int position = Wire.read();
+  int arg1 = Wire.read();
+  int arg2 = Wire.read();
+
+  int position;
 
   if (cmd == 0)
     return;
@@ -78,9 +91,10 @@ void receiveEvent(int howMany)
   lastAction = millis();
   Servo *servo;
   
-  if (servoNo == 1) {
+  if (cmd == CMD_STEP || cmd == CMD_MOVE_TO) {
+    if (arg1 == 1) {
     servo = &hServo;
-
+  
     if (cmd == CMD_STEP){
       if (position == 1) {
         newPosH++;
@@ -116,12 +130,51 @@ void receiveEvent(int howMany)
     }
     newPosV = position;
   }
+    int position = arg2;
+
+    if (arg1 == 1) {
+      servo = &hServo;
+
+      if (cmd == CMD_STEP) {
+        if (position == 1) {
+          newPosH++;
+        } else if (position == 0) {
+          --newPosH;
+        }
+        position = newPosH;
+      }
+      if (position > hMax) {
+        position = hMax;
+      }
+      if (position < hMin) {
+        position = hMin;
+      }
+      newPosH = position;
+    } else {
+      servo = &vServo;
+
+      if (cmd == CMD_STEP) {
+        if (position == 1) {
+          newPosV++;
+        } else if (position == 0) {
+          --newPosV;
+        }
+        position = newPosV;
+      }
+      if (position > vMax) {
+        position = vMax;
+      } else if (position < vMin) {
+        position = vMin;
+      }
+      newPosV = position;
+    }
+  }
+
 
   if (cmd == CMD_MOVE_TO || cmd == CMD_STEP) {
     idleMove = false;
     (*servo).write(position);
   } else if (cmd == CMD_SET_IDLE) {
-    resetPos = true;
     idleMove = true;
   } else if (cmd == CMD_RESET_POS) {
     idleMove = false;
@@ -130,48 +183,87 @@ void receiveEvent(int howMany)
     autoIdle = true;
   } else if (cmd == CMD_AUTO_IDLE_OFF) {
     autoIdle = false;
+  } else if (cmd == CMD_IDLE_AXIS) {
+    bool *axisIdle;
+    if (arg1 == 1) {
+      axisIdle = &idleH;
+    } else {
+      axisIdle = &idleV;
+    }
+
+    if (arg2 == 1) {
+      *axisIdle = true;
+    } else {
+      *axisIdle = false;
+    }
+  } else if (cmd == CMD_IDLE_SPEED) {
+    idleSpeed = 10 * arg1 + arg2;
+  } else if (cmd == CMD_STOP) {
+    idleMove = false;
   }
 }
 
 void runIdle()
 {
-  if (idleMoveRight) {
-    if (newPosH <= hMax) {
-      hServo.write(newPosH);
-      newPosH++;
-    } else {
-      newPosH = hMax;
-      idleMoveRight = false;
+  if (idleH) {
+    if (idleMoveRight)
+    {
+      if (newPosH <= hMax)
+      {
+        hServo.write(newPosH);
+        newPosH++;
+      }
+      else
+      {
+        newPosH = hMax;
+        idleMoveRight = false;
+      }
     }
-  } else {
-    if (newPosH >= hMin) {
-      hServo.write(newPosH);
-      --newPosH;
-    } else {
-      newPosH = hMin;
-      idleMoveRight = true;
-    }
-  }
-
-  if (idleMoveUp) {
-    if (newPosV <= vMax) {
-      vServo.write(newPosV);
-      newPosV++;
-    } else {
-      newPosV = vMax;
-      idleMoveUp = false;
-    }
-  } else {
-    if (newPosV >= vMin) {
-      vServo.write(newPosV);
-      --newPosV;
-    } else {
-      newPosV = vMin;
-      idleMoveUp = true;
+    else
+    {
+      if (newPosH >= hMin)
+      {
+        hServo.write(newPosH);
+        --newPosH;
+      }
+      else
+      {
+        newPosH = hMin;
+        idleMoveRight = true;
+      }
     }
   }
-
-  delay(300);
+  
+  if (idleV) {
+    if (idleMoveUp)
+    {
+      if (newPosV <= vMax)
+      {
+        vServo.write(newPosV);
+        newPosV++;
+      }
+      else
+      {
+        newPosV = vMax;
+        idleMoveUp = false;
+      }
+    }
+    else
+    {
+      if (newPosV >= vMin)
+      {
+        vServo.write(newPosV);
+        --newPosV;
+      }
+      else
+      {
+        newPosV = vMin;
+        idleMoveUp = true;
+      }
+    }
+  }
+  
+  delay(idleSpeed);
 }
 
 void reset() {
